9d38cc21a33033709f7669e353c64a75c88e25dc
gmahechas <gmahechas@ingenieros.com>
2021-01-12 14:24:17 -0500

    facebook login done

--- login-facebook/src/app/login-button/login-button.component.ts
+++ login-facebook/src/app/login-button/login-button.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var FB: any;
+
 @Component({
   selector: 'app-login-button',
   templateUrl: './login-button.component.html',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginButtonComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+    FB.getLoginStatus(function (response) {
+      console.log('STATUS:::', response);
+    });
   }
 
+  handleFBLogin() {
+    FB.login((response: any) => {
+      console.log('submitLogin', response);
+      if (response.authResponse) {
+        console.log('login successful', 'Success!');
+      } else {
+        console.log('User login failed');
+      }
+    }, {
+      scope: 'email,pages_manage_cta,pages_messaging,public_profile',
+      return_scopes: true
+    });
+  };
+
+  handleFBLogout() {
+    FB.logout(function (response) {
+      console.log('LogOut', response);
+    });
+  }
 }
