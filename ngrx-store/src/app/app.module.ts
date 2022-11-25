5ddd69e8056d897002ed1bf3bdffef6827443ccb
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-11-25 11:48:28 -0500

    init 2

--- ngrx-store/src/app/app.module.ts
+++ ngrx-store/src/app/app.module.ts
@@ -9,9 +9,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
 import {MatTabsModule} from '@angular/material/tabs';
-import {CourseComponent} from './course/course.component';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
@@ -24,30 +22,15 @@ import {MatSortModule} from '@angular/material/sort';
 import {MatTableModule} from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {HttpClientModule} from '@angular/common/http';
-import {CourseDialogComponent} from './course-dialog/course-dialog.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {LoginComponent} from './login/login.component';
-import {LessonComponent} from './lesson/lesson.component';
-import {SafeUrlPipe} from './common/safe-url.pipe';
-import {MessagesComponent} from './messages/messages.component';
-import {SearchLessonsComponent} from './search-lessons/search-lessons.component';
-import { LoadingComponent } from './loading/loading.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
-    CourseComponent,
-    CourseDialogComponent,
-    LoginComponent,
-    LessonComponent,
-    SafeUrlPipe,
-    MessagesComponent,
-    SearchLessonsComponent,
-    LoadingComponent
-
   ],
   imports: [
     BrowserModule,
@@ -71,12 +54,14 @@ import { LoadingComponent } from './loading/loading.component';
     MatSelectModule,
     MatDatepickerModule,
     MatMomentDateModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
   ],
   providers: [
   ],
   bootstrap: [AppComponent],
-  entryComponents: [CourseDialogComponent]
+  entryComponents: []
 })
 export class AppModule {
 }
--- ngrx-store/src/app/home/home.component.ts
+++ ngrx-store/src/app/home/home.component.ts
@@ -1,11 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -14,44 +7,12 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 })
 export class HomeComponent implements OnInit {
 
-  beginnerCourses: Course[];
+  beginnerCourses = [];
+  advancedCourses = [];
 
-  advancedCourses: Course[];
+  constructor() { }
 
-
-  constructor(private http: HttpClient, private dialog: MatDialog) {
-
-  }
-
-  ngOnInit() {
-
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
-
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
-  }
-
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-  }
+  ngOnInit() { }
 
 }
 
