4b1e4d02a46519725f46aac8f66a5a235b6763a0
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-11-26 18:23:43 -0500

    view course

--- ngrx-store/src/app/modules/courses/containers/view-courses/view-courses.component.ts
+++ ngrx-store/src/app/modules/courses/containers/view-courses/view-courses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { getSelectedCourse } from '../../store/selectors';
 import { Course } from '../../models/course.model';
 
 @Component({
@@ -8,10 +12,14 @@ import { Course } from '../../models/course.model';
 })
 export class ViewCoursesComponent implements OnInit {
 
-  course: Course;
-  constructor() { }
+  course$: Observable<Course>;
+
+  constructor(
+    private store: Store,
+  ) { }
 
   ngOnInit(): void {
+    this.course$ = this.store.select(getSelectedCourse);
   }
 
 }
